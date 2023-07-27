7d7a4f2237071faeb0e2c1eb27f187260673fd0e
Thiago C. dos Santos <thiagocarretero433@gmail.com>
2023-07-27 20:42:20 -0300

    ajuste

--- src/main/java/br/com/thiago/dto/PersonVo.java
+++ src/main/java/br/com/thiago/dto/PersonVo.java
@@ -1,13 +1,9 @@
 package br.com.thiago.dto;
 
 import br.com.thiago.model.Person;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
-@JsonPropertyOrder(value = {"id", "address", "firstName", "lastName", "gender"}) // muda a ordem
-public record PersonVo(Long id, @JsonProperty("primeiro_nome") String firstName, String lastName, String address,
-                       @JsonIgnore String gender) {
+
+public record PersonVo(Long id, String firstName, String lastName, String address, String gender) {
 
     public PersonVo(Person person) {
         this(person.getId(), person.getFirstName(), person.getLastName(), person.getAddress(), person.getGender());
